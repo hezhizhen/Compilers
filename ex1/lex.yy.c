2c68ff2ce39cb7a75af31478c5abc4117830a072
hezhizhen <hezhizhen.yi@gmail.com>
2015-04-09 11:34:42 +0800

    改进：一次能统计多个文件

--- ex1/lex.yy.c
+++ ex1/lex.yy.c
@@ -1757,16 +1757,33 @@ void yyfree (void * ptr )
 
 int main(int argc, char** argv)
 {
-    if(argc > 1)
+    int i, totchars = 0, totwords = 0, totlines = 0;
+    if(argc < 2) /* just read stdin */
     {
-        if(!(yyin = fopen(argv[1],"r")))
+        yylex();
+        printf("%8d%8d%8d\n", lines, words, chars);
+        return 0;
+    }
+    for(i = 1;i < argc; i++)
+    {
+        FILE *f = fopen(argv[i],"r");
+        if(!f)
         {
-            perror(argv[1]);
+            perror (argv[i]);
             return 1;
         }
+        yyrestart(f);
+        yylex();
+        fclose(f);
+        printf("%8d%8d%8d %s\n",lines, words, chars, argv[i]);
+        totchars += chars; chars = 0;
+        totwords += words; words = 0;
+        totlines += lines; lines = 0;
     }
-    yylex();
-    printf("%8d%8d%8d\n", liens, words, chars);
+    if(argc > 1)
+    {
+        printf("%8d%8d%8d total\n", totlines, totwords, totchars);
+    }
     return 0;
 }
 
